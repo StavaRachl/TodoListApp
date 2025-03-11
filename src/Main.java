aa4131f88ebd17cfdb2a6c5c68c455cbeb839c63
StavaRachl <zeltysevr121@gmail.com>
2025-03-11 20:49:56 +1000

    ъх

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Task> tasks = new ArrayList<>();
+        ArrayList<Task> tasks;
         Scanner scanner = new Scanner(System.in);
         boolean run = true;
+
+        tasks = Task.loadTask();
 
         while (run) {
             System.out.println("\n--- To-Do List ---");
@@ -13,7 +15,8 @@ public class Main {
             System.out.println("2. Добавить задачу");
             System.out.println("3. Отметить задачу как выполненную");
             System.out.println("4. Удалить задачу");
-            System.out.println("5. Выйти");
+            System.out.println("5. Сохранить задачу");
+            System.out.println("6. Выйти");
             System.out.println("Выберите действие: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -25,9 +28,12 @@ public class Main {
                 Task.markTaskCompleted(tasks, scanner);
             } else if (choice == 4) {
                 Task.deleteTask(tasks, scanner);
-            } else if (choice == 5){
-                run = false;
+            } else if (choice == 5) {
+                Task.saveTask(tasks);
+            } else if (choice == 6){
+                Task.saveTask(tasks);
                 System.out.println("Выход из приложения...");
+                run = false;
             } else {
                 System.out.println("Неверный ввод. Попробуйте снова.");
             }
--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -56,5 +57,26 @@ public class Task {
         } else {
             System.out.println("Неверный номер задачи.");
         }
+    }
+
+    static void saveTask(ArrayList<Task> tasks) {
+      try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("task.txt"))) {
+          oos.writeObject(tasks);
+          System.out.println("Задачи сохранены.");
+      } catch (IOException e) {
+          System.out.println("Произошла ошибка " + e.getMessage());
+      }
+    }
+
+    static ArrayList<Task> loadTask() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("task.txt"))) {
+            tasks = (ArrayList<Task>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден, создание файла произойдет автоматически");
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка " + e.getMessage());
+        }
+        return tasks;
     }
 }
